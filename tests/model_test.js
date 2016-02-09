5cdbc8d7c554fbc2d16d6865c13eef70f9356b7d
Pedro Januário <prnjanuario@gmail.com>
2016-02-09 05:59:36 +0000

    added model generator

---
+++ tests/model_test.js
@@ -0,0 +1,51 @@
+var chai = require('chai');
+    should = chai.should();
+
+describe('model', function(){
+  var target, config, metadata;
+
+  beforeEach(function(){
+    metadata = require('./metadata/index');
+    config = {
+      runtimeConfig: { baseUrl: 'http://test' },
+      metadata: metadata
+    };
+    target = require('../lib/model')(config, metadata.user);
+  });
+
+  it('should set model properties', function(){
+    var actual = target({ id: '1', name: 'Peter'});
+    actual.should.have.property('id', '1');
+    actual.should.have.property('name', 'Peter');
+  });
+
+  it('should set whitelisted properties only', function(){
+    var actual = target({ id: '1', something: 'option'});
+    actual.should.not.have.property('something');
+  });
+
+  it('should set defaults on unexistent properties', function(){
+    target({ id: '1' }).should.have.property('name', null);
+  });
+
+  describe('links', function(){
+    it('should set self link', function(){
+      var actual = target({ id: '1', name: 'Peter'});
+      actual.should.have.property('_links');
+      actual._links.should.have.property('self', 'http://test/users/1');
+    });
+
+    it('should set resource relation link', function(){
+      target = require('../lib/model')(config, metadata.task);
+      var actual = target({ id: '2', userId: '1' });
+      actual.should.have.property('_links');
+      actual._links.should.have.property('user', 'http://test/users/1');
+    });
+
+    it('should set collection relation link', function(){
+      var actual = target({ id: '1', name: 'Peter'});
+      actual.should.have.property('_links');
+      actual._links.should.have.property('tasks', 'http://test/users/1/tasks');
+    });
+  });
+});
