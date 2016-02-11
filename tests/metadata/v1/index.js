f69c10819963740b43e65837eb21a39a59e82f2f
Pedro Januário <prnjanuario@gmail.com>
2016-02-11 22:32:45 +0100

    added version support to metadata loader

--- tests/metadata/v1/index.js
+++ tests/metadata/v1/index.js
@@ -1,16 +1,18 @@
-var fs = require('fs');
+var fs = require('fs'),
+    path = require('path');
 
 /**
 * This is a clever convenience for accessing our custom metadata.
 * In your app, you can just do something like:
 *
 *     var metadata = require('./metadata/index');
-*     var v1Metadata = metadata.v1;
+*     var modelMetadata = metadata.model;
 *
 * Inspired by the way Express/Connect loads middleware.
 */
 fs.readdirSync(__dirname).forEach(function(filename) {
   if (filename === 'index.js') { return; }
-  function load(){ return require('./' + filename + '/index'); }
-  exports.__defineGetter__(filename, load);
+  var modelName = path.basename(filename, '.json');
+  function load(){ return require('./' + modelName); }
+  exports.__defineGetter__(modelName, load);
 });
--- tests/metadata_test.js
+++ tests/metadata_test.js
@@ -6,65 +6,76 @@ describe('metadata', function(){
   });
 
   describe('#load', function(){
+    it('should set version property', function(){
+      target('v1', 'task', {}).should.have.property('version', 'v1');
+    });
+
     it('should set model name property', function(){
-      target('task', {}).should.have.property('modelName', 'task');
+      target('v1', 'task', {}).should.have.property('modelName', 'task');
     });
 
     it('should set actions to empty array when unexistent', function(){
-      target('task', {}).actions.should.deep.equal([]);
+      target('v1', 'task', {}).actions.should.deep.equal([]);
     });
 
     it('should set relations to empty array when unexistent', function(){
-      target('task', {}).relations.should.deep.equal([]);
+      target('v1', 'task', {}).relations.should.deep.equal([]);
     });
 
     describe('on loading relation', function(){
+      it('should set relation version', function(){
+        var relation = { type: 'resource', name: 'user' };
+        var metadata = { relations: [ relation ] };
+        target('v1', 'task', metadata).relations[0]
+          .should.have.property('version', 'v1');
+      });
+
       it('should set relation model', function(){
         var relation = { type: 'resource', name: 'user' };
         var metadata = { relations: [ relation ] };
-        target('task', metadata).relations[0]
+        target('v1', 'task', metadata).relations[0]
           .should.have.property('model', 'user');
       });
 
       it('should set relation model sigularized', function(){
         var relation = { type: 'collection', name: 'tasks' };
         var metadata = { relations: [ relation ] };
-        target('user', metadata).relations[0]
+        target('v1', 'user', metadata).relations[0]
           .should.have.property('model', 'task');
       });
 
       it('should override relation model default', function(){
         var relation = { type: 'resource', name: 'owner', model: 'user' };
         var metadata = { relations: [ relation ] };
-        target('task', metadata).relations[0]
+        target('v1', 'task', metadata).relations[0]
           .should.have.property('model', 'user');
       });
 
       it('should set relation parent', function(){
         var relation = { type: 'resource', name: 'user' };
         var metadata = { relations: [ relation ] };
-        target('task', metadata).relations[0]
+        target('v1', 'task', metadata).relations[0]
           .should.have.property('parent', 'task');
       });
 
       it('should set relation model foreign key', function(){
         var relation = { type: 'resource', name: 'user' };
         var metadata = { relations: [ relation ] };
-        target('task', metadata).relations[0]
+        target('v1', 'task', metadata).relations[0]
           .should.have.property('modelFk', 'userId');
       });
 
       it('should set relation foreign key for collection', function(){
         var relation = { type: 'collection', name: 'tasks' };
         var metadata = { relations: [ relation ] };
-        target('user', metadata).relations[0]
+        target('v1', 'user', metadata).relations[0]
           .should.have.property('modelFk', 'userId');
       });
 
       it('should override relation model foreign key default', function(){
         var relation = { type: 'resource', name: 'owner', modelFk: 'userId' };
         var metadata = { relations: [ relation ] };
-        target('task', metadata).relations[0]
+        target('v1', 'task', metadata).relations[0]
           .should.have.property('modelFk', 'userId');
       });
     });
